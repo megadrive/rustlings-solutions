6655b5420745e43643997b0780ffcf61a63464d4
Andy <a.simeon@gmail.com>
2022-08-25 21:58:38 +1000

    thing

---
+++ exercises/functions/functions1.rs
@@ -0,0 +1,10 @@
+// functions1.rs
+// Execute `rustlings hint functions1` or use the `hint` watch subcommand for a hint.
+
+fn call_me(name: &str) {
+    println!("Hello {}!", name);
+}
+
+fn main() {
+    call_me("Andy");
+}
